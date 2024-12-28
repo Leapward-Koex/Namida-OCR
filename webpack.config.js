428d75e7a94ae8a707231a929b62e940cecf0b07
LeapwardKoex <sot838@gmail.com>
2024-12-29 11:01:57 +1300

    Add browser specific builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,73 @@
 const path = require('path');
+const fs = require('fs');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        background: './src/background/index.ts',
-        content: './src/content/index.ts',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name]/index.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    },
-    module: {
-        rules: [
+module.exports = (env) => {
+    const browser = env.browser || 'firefox';
+
+    const createManifest = () => {
+        const broswerSpecificManifest = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, `manifests/manifest.${browser}.json`), 'utf8')
+        );
+
+        const baseManifest = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'manifests/manifest.base.json'), 'utf8')
+        );
+
+        fs.writeFileSync(
+            path.resolve(__dirname, 'dist/manifest.json'),
+            JSON.stringify(merged = { ...baseManifest, ...broswerSpecificManifest }, null, 2)
+        );
+    };
+    return {
+        mode: 'development',
+        entry: {
+            background: './src/background/index.ts',
+            content: './src/content/index.ts',
+            ui: './src/ui/index.ts',
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name]/index.js',
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: 'src/ui/popup.html', to: 'ui/popup.html' },
+                    { from: 'src/ui/styles.css', to: 'ui/styles.css' },
+                    // https://github.com/naptha/tesseract.js/blob/master/docs/local-installation.md#local-installation
+                    { from: 'node_modules/tesseract.js-core/tesseract-core.wasm.js', to: 'libs/tesseract-core/tesseract-core.wasm.js' },
+                    { from: 'node_modules/tesseract.js-core/tesseract-core-simd.wasm.js', to: 'libs/tesseract-core/tesseract-core-simd.wasm.js' },
+                    { from: 'node_modules/tesseract.js-core/tesseract-core-lstm.wasm.js', to: 'libs/tesseract-core/tesseract-core-lstm.wasm.js' },
+                    { from: 'node_modules/tesseract.js-core/tesseract-core-simd-lstm.wasm.js', to: 'libs/tesseract-core/tesseract-core-simd-lstm.wasm.js' },
+                    { from: 'node_modules/tesseract.js/dist/worker.min.js', to: 'libs/tesseract-worker/worker.min.js' },
+                    { from: 'lang/', to: 'libs/tesseract-lang/' },
+                    { from: 'node_modules/@upscalerjs/esrgan-medium/models/', to: 'libs/tensorflow/' },
+
+                ],
+            }),
             {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
+                apply: (compiler) => {
+                    compiler.hooks.afterEmit.tap('GenerateManifestPlugin', (compilation) => {
+                        compilation.fileDependencies.add(path.resolve(__dirname, 'manifests/manifest.base.json'));
+                        compilation.fileDependencies.add(path.resolve(__dirname, `manifests/manifest.${browser}.json`));
+                        createManifest();
+                    });
+                },
             },
         ],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: 'src/manifest.json', to: 'manifest.json' },
-                // https://github.com/naptha/tesseract.js/blob/master/docs/local-installation.md#local-installation
-                { from: 'node_modules/tesseract.js-core/tesseract-core.wasm.js', to: 'libs/tesseract-core/tesseract-core.wasm.js' },
-                { from: 'node_modules/tesseract.js-core/tesseract-core-simd.wasm.js', to: 'libs/tesseract-core/tesseract-core-simd.wasm.js' },
-                { from: 'node_modules/tesseract.js-core/tesseract-core-lstm.wasm.js', to: 'libs/tesseract-core/tesseract-core-lstm.wasm.js' },
-                { from: 'node_modules/tesseract.js-core/tesseract-core-simd-lstm.wasm.js', to: 'libs/tesseract-core/tesseract-core-simd-lstm.wasm.js' },
-                { from: 'node_modules/tesseract.js/dist/worker.min.js', to: 'libs/tesseract-worker/worker.min.js' },
-                { from: 'lang/', to: 'libs/tesseract-lang/' },
-                { from: 'node_modules/@upscalerjs/esrgan-medium/models/', to: 'libs/tensorflow/' },
-
-            ],
-        }),
-    ],
-    devtool: 'source-map',
-};
+        devtool: 'source-map',
+    };
+}
