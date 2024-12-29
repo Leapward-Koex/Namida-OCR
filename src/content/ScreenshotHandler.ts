bd069d6ed623ea4914909d8b46f2f7879c3a7f0c
LeapwardKoex <sot838@gmail.com>
2024-12-29 13:46:54 +1300

    Add crossorigin

--- src/content/ScreenshotHandler.ts
+++ src/content/ScreenshotHandler.ts
@@ -51,6 +51,7 @@ export class ScreenshotHandler {
     private loadImage(src: string): Promise<HTMLImageElement> {
         return new Promise((resolve, reject) => {
             const img = new Image();
+            img.crossOrigin = "anonymous";
             img.onload = () => resolve(img);
             img.onerror = reject;
             img.src = src;
